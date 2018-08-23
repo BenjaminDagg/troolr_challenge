31321843ddd83e9930d6d6d0465e3dff0dab5c87
unknown <bdagg@csumb.edu>
2018-08-22 21:47:23 -0700

    createde logo component on home page

--- troolr-dagg/src/components/Logo/Logo.js
+++ troolr-dagg/src/components/Logo/Logo.js
@@ -1,2 +1,22 @@
 import React, { Component } from 'react';
 import './Logo.css';
+
+/*
+This component is the troolr logo. When clicked
+returns to the home page
+
+ */
+
+export class Logo extends Component {
+    render() {
+        return (
+            <div id="home-logo">
+                <a href="/">
+                    <img style={{'height':'auto', 'width':'150px'}} src="https://troolr.com/assets/graphics/logo-small.png" />
+                </a>
+            </div>
+        );
+    }
+}
+
+
