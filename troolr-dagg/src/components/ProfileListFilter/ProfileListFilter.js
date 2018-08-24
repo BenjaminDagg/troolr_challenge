15b818972500dbc56c363b50515abf9fb1014af6
unknown <bdagg@csumb.edu>
2018-08-23 21:28:26 -0700

    added styling to filter button

--- troolr-dagg/src/components/ProfileListFilter/ProfileListFilter.js
+++ troolr-dagg/src/components/ProfileListFilter/ProfileListFilter.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { FilterTypes } from "../../models/FilterTypes";
-
+import "./ProfileListFilter.css"
 
 /*
 This component is a drop down selector that allows the user
@@ -15,7 +15,12 @@ export class ProfileListFilter extends Component {
     constructor(props) {
         super(props);
 
+        this.state = {
+            filtersVisible: false
+        };
+
         this.handleFilterChange = this.handleFilterChange.bind(this);
+        this.toggleFilters = this.toggleFilters.bind(this);
     }
 
     /*
@@ -25,18 +30,42 @@ export class ProfileListFilter extends Component {
     handleFilterChange(event) {
         //calling parent callback passed as a prop
         this.props.onFilterChanged(event.target.value);
+
+        this.toggleFilters();
+    }
+
+    toggleFilters() {
+        if (!this.state.filtersVisible) {
+            document.getElementById('filters').style.display = "block";
+            this.setState({filtersVisible: true});
+        }
+        else {
+            document.getElementById('filters').style.display = "none";
+            this.setState({filtersVisible: false});
+        }
     }
 
     render() {
 
-
+        /*
         return (
             <div  id="list-filter">
-                <select value={this.props.filters} onChange={this.handleFilterChange}>
-                    <option>Sort By ({this.props.filter})</option>
-                    <option value={FilterTypes.TOP_RATED}>{FilterTypes.TOP_RATED}</option>
-                    <option value = {FilterTypes.VIEW_COUNT}>{FilterTypes.VIEW_COUNT}</option>
+                <select id="filter" value={this.props.filters} onChange={this.handleFilterChange}>
+                    <option class="filter-option">Sort By ({this.props.filter})</option>
+                    <option class="filter-option" value={FilterTypes.TOP_RATED}>{FilterTypes.TOP_RATED}</option>
+                    <option class="filter-option" value = {FilterTypes.VIEW_COUNT}>{FilterTypes.VIEW_COUNT}</option>
                 </select>
+            </div>
+        );
+        */
+        return (
+            <div id="list-filter">
+                <button onClick={this.toggleFilters} id="filterbtn">Sort by ({this.props.filter}) &#9660;</button>
+                <div id="filters">
+                    <button class="dropdown-btn" onClick={this.handleFilterChange} value={FilterTypes.TOP_RATED}>Top Rated</button>
+                    <br/>
+                    <button class="dropdown-btn" onClick={this.handleFilterChange} value={FilterTypes.VIEW_COUNT} >Most Viewed</button>
+                </div>
             </div>
         );
     }
